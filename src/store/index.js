a1266de5715ecd587b05ad79c0632932d87df328
Hari <harikrishkk@gmail.com>
2022-08-14 14:10:58 -0400

    add middlewares

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,22 @@
 import { legacy_createStore as createStore, compose } from 'redux';
-import { combineReducers } from 'redux';
+import { combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import productsReducer from './products';
 import { addLoggingOnDispatch, addAppVersion } from '../add-ons/enhancers';
+import { blockActionMW, loggerMiddleware } from '../add-ons/middlewares';
 
 const rootReducer = combineReducers({
   products: productsReducer,
 });
 
 const composedEnhancer = compose(addLoggingOnDispatch, addAppVersion);
+const middlewareEnhancer = applyMiddleware(blockActionMW, loggerMiddleware);
 
 // CREATE A STORE
 const store = createStore(
   rootReducer,
   undefined,
-  composeWithDevTools(composedEnhancer)
+  composeWithDevTools(composedEnhancer, middlewareEnhancer)
 );
 
 console.log('MODIFIED WITH ENHANCER --> ', store.getState());
