0ffd97a4f28a979443f0e09fe95b9b175389133f
Hari <harikrishkk@gmail.com>
2022-08-17 21:41:47 -0400

    add favorite redux slice

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import FavoriteItem from '../components/FavoriteItem';
 
 const Favorites = () => {
+  const favoriteIds = useSelector((state) => state.favorites);
+  const favItems = useSelector((state) => {
+    return favoriteIds.map((id) =>
+      state.products.find((item) => item.id === id)
+    );
+  });
   return (
     <ul className="list-group">
-      <FavoriteItem />
+      {favItems.map((item) => (
+        <FavoriteItem key={item.id} item={item} />
+      ))}
     </ul>
   );
 };
--- src/store/favorites.js
+++ src/store/favorites.js
@@ -3,7 +3,17 @@ const INIT_STATE = [];
 function favoritesReducer(state = INIT_STATE, action) {
   switch (action.type) {
     case 'favorites/ADD_FAVORITE':
-      return [...state, action.payload];
+      // if empty state, just add
+      if (state.length === 0) {
+        return [action.payload];
+      }
+      // if it already exist, then toggle it. Else add it
+      const isExisting = !!state.find((item) => item === action.payload);
+      if (isExisting) {
+        return state.filter((item) => item !== action.payload);
+      }
+      return state.concat(action.payload);
+
     default:
       return state;
   }
