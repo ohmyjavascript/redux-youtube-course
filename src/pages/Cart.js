2c9f959350e462279cb91854dc8c19ceaf7b1269
Hari <harikrishkk@gmail.com>
2022-08-21 11:17:58 -0400

    adding notification bar for favorites & cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,10 +2,23 @@ import React from 'react';
 import CartItem from '../components/CartItem';
 import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../store/cart';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
+
+  if (cartItems.length === 0) {
+    return (
+      <div className="alert alert-dismissible alert-info">
+        <strong>Alert! </strong> Please start adding to cart
+        <Link to="/" className="ml-2 alert-link">
+          from here
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <ul className="list-group">
       {cartItems.length &&
